73a929d81dc863428b55f0e97570ccdcdbedb8fa
D-Stukes <dstukes888@gmail.com>
2023-02-06 15:26:29 -0500

    reviewed and updated coded to use index instead of id

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -3,22 +3,13 @@
 const express = require("express")
 const transactions = express.Router()
 const transactionsArray = require('../models/transactions.js')
-
 const { validateURL } = require('../models/validations.js')
 
 
-//GET ROUTE FOR /transactions (app.use in app.js handles the transactions)
+//GET ROUTE 
 transactions.get('/', (req, res) =>{
     res.json(transactionsArray)
 })
-
-//POST ROUTE - transactions endpoint for POST
-transactions.post("/",validateURL, (req, res)=>{
-    // const itemID = uuid()
-    // req.body[transactionId] = itemID
-    transactionsArray.push(req.body)
-    res.json(transactionsArray.at(-1))
-})  //at -1 means end of array length - 1, so it will display the last element posted
 
 
 //SHOW ROUTE
@@ -32,6 +23,29 @@ transactions.get('/:index', (req, res) =>{
     }
 })
 
+
+
+
+//POST ROUTE - transactions endpoint for POST
+transactions.post("/",validateURL, (req, res)=>{
+    // const itemID = uuid()
+    // req.body[transactionId] = itemID
+    transactionsArray.push(req.body)
+    res.json(transactionsArray.at(-1))
+})  //at -1 means end of array length - 1, so it will display the last element posted
+
+
+//PUT ROUTE
+transactions.put("/:index", (req,res) => {
+    const {index} = req.params
+    if(transactionsArray[index]) {
+        transactionsArray[index] = req.body
+        res.status(200).json(transactionsArray[req.params.index])
+    } else {
+        res.status(404).json({message: "Not Found"})
+    }
+   })
+
 //DELETE ROUTE
 transactions.delete("/:index", (req,res) => {
     const { index } = req.params
@@ -40,15 +54,5 @@ transactions.delete("/:index", (req,res) => {
 })
 
 
-//PUT ROUTE
-transactions.put("/:index", (req,res) => {
-    const {index} = req.params
-    if(transactionsArray[index]) {
-        transactionsArray[index] =req.body
-        res.status(200).json(transactionsArray[index])
-    } else {
-        res.status(404).json({message: "Not Found"})
-    }
-   })
 
    module.exports = transactions
